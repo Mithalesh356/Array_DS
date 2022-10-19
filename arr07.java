49f6ec5a9274f7c6eb6cc802d002fa377ed883b3
Divya Gaurav <divyag.ug16.ee@nitp.ac.in>
2022-10-19 15:21:12 +0530

    binary search , searching in array

---
+++ arr07.java
@@ -0,0 +1,30 @@
+//PROBLEM
+//    (PAIRSUM)
+//    find and return the total number of pairs count
+//    		in the array which sum is equal to X
+
+
+package Array;
+
+public class arr07 {
+
+	public static int pairSum(int arr[] , int x) {
+		int pairCount = 0;
+		for(int i=0; i< arr.length-1; i++) {
+			for(int j=i+1; j< arr.length; j++) {
+				if(arr[i]+arr[j] == x) {
+					pairCount++;
+				}
+			}
+		}
+		return pairCount;	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int A[] = {1,2,3,4};
+		int paircount = pairSum(A, 5);
+		System.out.println(paircount);
+
+	}
+
+}
---
+++ arr08.java
@@ -0,0 +1,36 @@
+//PROBLEM
+//   (binary search)
+//   we have to find element x in the given SORTED
+//   array if it present return its index otherwise return -1
+
+
+package Array;
+
+public class arr08 {
+
+	public static int binarySearch(int arr[] , int x) {
+		int si = 0;
+		int ei = arr.length-1;
+		while(si<=ei) {
+			int mid = (si+ei)/2;
+			if(arr[mid] == x) {
+				return mid;
+			}else if(arr[mid] < x) {
+				si = mid + 1;
+			}else {
+				ei = mid - 1;
+			}
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {1,3,4,6,8,9,11};
+		int findIndex = binarySearch(arr, 8);
+		System.out.println(findIndex);
+		
+
+	}
+
+}
