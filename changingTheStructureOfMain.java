2236cd14296f8a273df2eb001c604b2d5a3cc852
Mithalesh356 <112802546+Mithalesh356@users.noreply.github.com>
2023-07-11 14:07:33 +0530

    Add files via upload

---
+++ changingTheStructureOfMain.java
@@ -0,0 +1,8 @@
+import java.net.PortUnreachableException;
+
+public class changingTheStructureOfMain {
+    static public void main(String... args)
+    {
+        System.out.println("mk");
+    }
+}
